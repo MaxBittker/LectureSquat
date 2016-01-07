b8dfb479ce12a0e92dc9b48650cf9b9397feac27
maxbittker <maxbittker@gmail.com>
2016-01-07 14:37:55 -0500

    webpack output

--- index.js
+++ index.js
@@ -45,17 +45,43 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var request = __webpack_require__(1)
-	request('8.8.8.8', function(er, res) {
-	    if (!er)
-	        return console.log('browser-request got your root path:\n' + res.body)
 
-	    console.log('There was an error, but at least browser-request loaded and ran!')
-	    throw er
+	var timeSelect = document.getElementById("time");
+	var btn = document.getElementById('goButton');
+	var resultDiv = document.getElementById("results");
 
 
-	})
 
-	// console.log("test")
+	var getClasses = function(time) {
+	    request('https://postgrest.herokuapp.com/sessions', function(er, res) {
+	        if (!er) {
+	            var node = document.createElement("h4"); // Create a <li> node
+	            // console.log(res.body)
+	            var textnode = document.createTextNode(JSON.stringify(JSON.parse(res.body)[0])); // Create a text node
+	            node.appendChild(textnode); // Append the text to <li>
+	            resultDiv.appendChild(node); // Append <li> to <ul> with 
+	        } else {
+	            console.log('There was an error')
+	            throw er
+	        }
+	    })
+	}
+
+	btn.addEventListener('click', function() {
+	    var time = timeSelect.options[timeSelect.selectedIndex].value;
+
+	    getClasses(time);
+	}, false);
+
+
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+
+	if ((h < 8) || (h > 18)) {
+	    h = 8;
+	}
+	timeSelect.value = h.toString()
 
 
 /***/ },
